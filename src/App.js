fc0b8c204d618033a9e133e2f8fce84fc77cdca4
nimanthaF <mimanthafernando95@gmail.com>
2021-03-09 08:02:18 +0530

    creatin todos #1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import Todos from './components/Todos';
 
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component{
+
+  state={
+    todos:[
+      {
+        id:1,
+        title:'tkae out the trash',
+        completed:false
+      },
+      {
+        id:2,
+        title:'work out',
+        completed:false
+      },
+      {
+        id:3,
+        title:'go for a walk',
+        completed:false
+      }
+    ]
+  }
+
+  render(){
+
+    console.log(this.state.todos)
+
+    return(
+      <div className="App">
+        <Todos/>
+      </div>
+    );
+  }
 }
 
 export default App;
