be51b023944eb34f6b4ed508097c9ddf67530746
nimanthaF <nimanthafernando95@gmail.com>
2021-04-28 10:01:09 +0530

    adding the state

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+    state={
+        title:''
+    }
+
+    onChange=(e)=>this.setState(
+        {
+            [e.target.name]:e.target.value
+        }
+    );
     render() {
         return (
             <form>
@@ -8,7 +17,8 @@ export class AddTodo extends Component {
                     type='text'
                     name='title'
                     style={{flex:'10',padding:'5px'}}
-                    placeholder='Add Todo...'    
+                    placeholder='Add Todo...'  
+                    onChange={this.onChange}  
                 />
                 <input 
                     type='submit'
